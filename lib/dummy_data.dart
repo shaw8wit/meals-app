f232959698b630ffa7a7b23ff799dd9464b55390
shaw8wit <shaw8wit@gmail.com>
2019-10-31 22:12:22 +0530

    Dummy Tests

---
+++ lib/dummy_data.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const[
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'French',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Spanish',
+    color: Colors.lime,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'French',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Spanish',
+    color: Colors.lime,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'French',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Spanish',
+    color: Colors.lime,
+  ),
+];
